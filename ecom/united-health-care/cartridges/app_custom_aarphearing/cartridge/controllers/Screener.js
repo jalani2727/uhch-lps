1e7cf9bdfb8cc6f2b6b9542e5c7ee78e603d5df5
jalani2727 <jalani2727@gmail.com>
2023-08-18 13:30:58 -0400

    Update adobe data layer values - remove unnecessary datalayer templates

--- ecom/united-health-care/cartridges/app_custom_aarphearing/cartridge/controllers/Screener.js
+++ ecom/united-health-care/cartridges/app_custom_aarphearing/cartridge/controllers/Screener.js
@@ -16,28 +16,68 @@ var server = require('server');
 server.get(
     'ResultsPage1',
     function (req, res, next) {
+        var viewData = res.getViewData();
+
+        viewData.adobeDataLayer = {};
+        viewData.adobeDataLayer.Context = 'GlobalData'
+        viewData.adobeDataLayer.PageGroup = "Questionnaire Results 1";
+        viewData.adobeDataLayer.PageName = "questionnaire-results-1";
+
+        res.setViewData();
+        
         res.render('hearingScreener/results/q-results-1');
+        
         next();
     }
 );
 server.get(
     'ResultsPage2',
     function (req, res, next) {
+        var viewData = res.getViewData();
+
+        viewData.adobeDataLayer = {};
+        viewData.adobeDataLayer.Context = 'GlobalData'
+        viewData.adobeDataLayer.PageGroup = "Questionnaire Results 2";
+        viewData.adobeDataLayer.PageName = "questionnaire-results-2";
+
+        res.setViewData();
+
         res.render('hearingScreener/results/q-results-2');
+        
         next();
     }
 );
 server.get(
     'ResultsPage3',
     function (req, res, next) {
+        var viewData = res.getViewData();
+
+        viewData.adobeDataLayer = {};
+        viewData.adobeDataLayer.Context = 'GlobalData'
+        viewData.adobeDataLayer.PageGroup = "Questionnaire Results 3";
+        viewData.adobeDataLayer.PageName = "questionnaire-results-3";
+
+        res.setViewData();
+
         res.render('hearingScreener/results/q-results-3');
+        
         next();
     }
 );
 server.get(
     'ResultsPage4',
     function (req, res, next) {
+        var viewData = res.getViewData();
+
+        viewData.adobeDataLayer = {};
+        viewData.adobeDataLayer.Context = 'GlobalData'
+        viewData.adobeDataLayer.PageGroup = "Questionnaire Results 4";
+        viewData.adobeDataLayer.PageName = "questionnaire-results-4";
+
+        res.setViewData();
+
         res.render('hearingScreener/results/q-results-4');
+        
         next();
     }
 );
@@ -46,14 +86,34 @@ server.get(
 server.get(
     'Show',
     function (req, res, next) {
+        var viewData = res.getViewData();
+
+        viewData.adobeDataLayer = {};
+        viewData.adobeDataLayer.Context = 'GlobalData'
+        viewData.adobeDataLayer.PageGroup = "Questionnaire Widget";
+        viewData.adobeDataLayer.PageName = "questionnaire-widget";
+
+        res.setViewData();
+
         res.render('hearingScreener/questionnaire');
+        
         next();
     }
 );
 server.get(
     'ShowLanding',
     function (req, res, next) {
+        var viewData = res.getViewData();
+
+        viewData.adobeDataLayer = {};
+        viewData.adobeDataLayer.Context = 'GlobalData'
+        viewData.adobeDataLayer.PageGroup = "Questionnaire Landing Page";
+        viewData.adobeDataLayer.PageName = "questionnaire-landing-page";
+
+        res.setViewData();
+
         res.render('hearingScreener/questionnaireLanding');
+        
         next();
     }
 );
